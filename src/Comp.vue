0f8fcc29f3db2043dea34a142d5f226567dee56f
Robbin Baauw <robbin.baauw@gmail.com>
2020-03-30 17:39:16 +0200

    feat(events): showcase of mouse events

--- src/Comp.vue
+++ src/Comp.vue
@@ -2,7 +2,7 @@
     <node :scale="prop">
         <!--        <rect v-for="(item,index) in images" :x="item.x" :w="item.w" :y="index * 40" :h="20" :color="0xff00ffff">-->
         <!--        </rect>-->
-        <image src="./src/logo.png"></image>
+        <image src="./src/logo.png" @mousemove="log"></image>
     </node>
 </template>
 
@@ -19,7 +19,7 @@ export default {
             { x: 0, w: 800 },
             { x: 0, w: 400 },
         ]);
-        return { images };
+        return { images, log: (e: Event) => console.log(e) };
     },
 };
 </script>
