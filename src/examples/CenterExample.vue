bbd1a6f0d213ae2b3f75be3c482ebd7ef6123564
Bas van Meurs <bvanmeurs1985@gmail.com>
2020-04-21 21:08:36 +0200

    feat(upgrade)

--- src/examples/CenterExample.vue
+++ src/examples/CenterExample.vue
@@ -5,9 +5,9 @@
         <rectangle w="w" h="h" :color-left="0xffff0000" :color-right="0xff0000ff" />
 
         <!-- use mount to position an element relative to its size (can be used for centering with unknown width) -->
-        <text x="w/2" y="h/2" mount-x="0.5" mount-y="0.5">Centered</text>
-        <text :x="0" :y="0" mount-x="0" mount-y="0">Top left</text>
-        <text x="w" y="h" mount-x="1" mount-y="1">Bottom right</text>
+        <text x="w/2" y="h/2" :mount-x="0.5" :mount-y="0.5">Centered</text>
+        <text :x="0" :y="0" :mount-x="0" :mount-y="0">Top left</text>
+        <text x="w" y="h" :mount-x="1" :mount-y="1">Bottom right</text>
     </rectangle>
 </template>
 
